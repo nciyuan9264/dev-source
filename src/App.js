3b2c6780576a4ddeca12fdf0d6ccf56b4ce1b6a7
WangZhenyu <wangzhenyu.9264@bytedance.com>
2024-08-22 21:25:59 +0800

    fix: 调试信息

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,15 +11,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        {count}
       </header>
+      <div
+        onClick={() => {
+          setCount(prev => prev + 1);
+        }}
+      >+1</div>
     </div>
   );
 }
